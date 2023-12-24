b14550271d81d18bc29246c04bb763c8a9e5c593
Anusha <anushasharma824@gmail.com>
2023-12-24 18:35:09 +0530

    resonsive fixes

--- src/components/SkillCard.js
+++ src/components/SkillCard.js
@@ -2,7 +2,7 @@ import React from "react"
 
 export default function SkillCard({ name, logo }) {
     return (
-        <div className="flex flex-col justify-center m-5 lg:m-14 p-5 h-32 w-32 md:h-auto md:w-auto border border-black hover:bg-cyan-950 hover:scale-125">
+        <div className="flex flex-col justify-center m-2 lg:m-14 p-5 h-36 w-36 md:h-auto md:w-auto border border-black hover:bg-cyan-950 hover:scale-125">
             <img className="h-12 md:h-28 object-contain" src={logo} alt={name} />
             <p className="skills text-xl text-center">{name}</p>
         </div>
