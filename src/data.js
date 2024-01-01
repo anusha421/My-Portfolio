3ea20ba4ff3b2594d509a7ceb8f0fe69521f20d8
Anusha <anushasharma824@gmail.com>
2024-01-01 12:49:17 +0530

    projects update

--- src/data.js
+++ src/data.js
@@ -1,7 +1,7 @@
 import tasks from "./assets/tasks.jpeg";
 import commerce from "./assets/commerce.png";
-import giddygifs from "./assets/giddygifs.jpeg";
-import css from "./assets/css.png"
+import fitnesshub from './assets/fitnesshub.png';
+import css from "./assets/css.png";
 import cpp from "./assets/cpp.png";
 import django from "./assets/django.png";
 import git from "./assets/git.png";
@@ -14,6 +14,14 @@ import sql from "./assets/sql.png";
 
 export const projects = [
   {
+    title: "Fitness Hub",
+    subtitle: "React, Material UI",
+    description:
+      "An exercise ideas web application used to find new gym inspiration and learn new exercises.",
+    image: fitnesshub,
+    link: "https://fitness-hubb.netlify.app/",
+  },
+  {
     title: "Tasks",
     subtitle: "Django, HTML and CSS",
     description:
@@ -24,34 +32,28 @@ export const projects = [
   {
     title: "Commerce",
     subtitle: "Django, HTML and CSS",
-    description:
-      "An eBay-like e-commerce auction site. Created using Django.",
+    description: "An eBay-like e-commerce auction site. Created using Django.",
     image: commerce,
     link: "https://youtu.be/v0pd3395iFI?si=yuyr-7te_lao6q1S",
-  },
-  {
-    title: "Giddy Gifs",
-    subtitle: "HTML, CSS, Bootstrap and Giphy's API",
-    description:
-      "A responsive web application used to display trending, random and any gif you search for. Created using HTML, CSS, Bootstrap and Giphy's API.",
-    image: giddygifs,
-    link: "https://www.youtube.com/watch?v=uz20vBS0CDE",
   },
 ];
 
 export const achievements = [
   {
     title: "CS50's Introduction to Computer Science",
-    description: "A basic computer course including topics like abstraction, algorithms,data structures, encapsulation, resource management, security, software engineering, and web programming. Languages include C, Python, and SQL plus HTML, CSS, and JavaScript.",
-    certificate: "https://cs50.harvard.edu/certificates/96e7327d-feb5-4be5-9a31-43e336aba980",
+    description:
+      "A basic computer course including topics like abstraction, algorithms,data structures, encapsulation, resource management, security, software engineering, and web programming. Languages include C, Python, and SQL plus HTML, CSS, and JavaScript.",
+    certificate:
+      "https://cs50.harvard.edu/certificates/96e7327d-feb5-4be5-9a31-43e336aba980",
   },
   {
     title: "CS50's Web Programming with Python and JavaScript",
-    description: "To learn about design and implementation of web apps with Python,  JavaScript, and SQL using frameworks like Django, React, and Bootstrap. Topics include database design, scalability, security, and user experience.",
-    certificate: "https://cs50.harvard.edu/certificates/4dca46d5-b1f6-4950-82a4-3b0710c4470f",
-  }
-]
-
+    description:
+      "To learn about design and implementation of web apps with Python,  JavaScript, and SQL using frameworks like Django, React, and Bootstrap. Topics include database design, scalability, security, and user experience.",
+    certificate:
+      "https://cs50.harvard.edu/certificates/4dca46d5-b1f6-4950-82a4-3b0710c4470f",
+  },
+];
 
 export const skills = [
   {
